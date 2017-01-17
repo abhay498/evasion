d1d87d4078bf4a5f0b9db6a184d72b7bc9fe8d27
Abhay Kumar Singh <abskumar798@gmail.com>
2017-01-17 12:33:13 +0530

    added more code to the game

--- game.py
+++ game.py
@@ -1,20 +1,113 @@
 import pygame
+import time
+import random
 
 pygame.init()
-gameDisplay = pygame.display.set_mode((800,600))
+
+display_width = 800
+display_height = 600
+
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Evade')
 clock = pygame.time.Clock()
 
-crashed = False
+carImg = pygame.image.load('reds.png')
 
-while not crashed:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
+car_width = 73
 
-    pygame.display.update()
-    clock.tick(30)
+def things_dodged(count):
+    font = pygame.font.SysFont(None,25)
+    text = font.render("Dodged:"+str(count),True,black)
+    gameDisplay.blit(text,(0,0))
     
+def things(thingx,thingy,thingw,thingh,color):
+    pygame.draw.rect(gameDisplay,color,[thingx,thingy,thingw,thingh])    
+
+def car(x,y):
+    gameDisplay.blit(carImg,(x,y))
+
+def text_objects(text,font):
+    textSurface = font.render(text,True,black)
+    return textSurface, textSurface.get_rect()
+    
+def crash():
+    message_display('You crashed')
+
+def message_display(text):
+    large_text = pygame.font.Font('freesansbold.ttf',115)
+    TextSurf, TextRect = text_objects(text,large_text)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf,TextRect)
+    pygame.display.update()
+    time.sleep(2)
+    game_loop()
+
+def game_loop():
+    x = (display_width * 0.45)
+    y = (display_height * 0.8)
+
+    x_change = 0
+    dodged = 0
+    thing_startx = random.randrange(0,display_width)
+    thing_starty = -600
+    thing_speed = 4
+    thing_width = 100
+    thing_height = 100
+    
+    gameExit = False
+
+    while not gameExit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = True
+                
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 5
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
+
+            
+        x += x_change            
+                
+        gameDisplay.fill(white)
+
+        #things(thingx,thingy,thingw,thingh,color):
+        things(thing_startx,thing_starty,thing_width,thing_height,black)
+        thing_starty += thing_speed
+        car(x,y)
+        things_dodged(dodged)
+        
+        if x > display_width - car_width or x < 0:
+            crash()
+
+        if thing_starty > display_height:
+            thing_starty = 0 - thing_height
+            thing_startx = random.randrange(0,display_width)
+            dodged += 1
+            thing_speed += 0.5
+            thing_width += (dodged *1.2)
+
+        if y < thing_starty + thing_height:
+            print('y crossover')
+
+            if x > thing_startx and x < thing_startx + thing_width or x + car_width > thing_startx and x + car_width < thing_startx + thing_width:
+                print('x crossover')
+                crash()
+                
+        pygame.display.update()
+        clock.tick(100)
+
+game_loop()    
 pygame.quit()
 quit()
 
